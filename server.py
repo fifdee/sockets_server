7b417a7c7fd7c25a3b3bfa4e235ed7bea01dd3de
fifde <fifdee@outlook.com>
2023-01-22 00:03:41 +0100

    added Response class, separated logics

--- server.py
+++ server.py
@@ -3,13 +3,69 @@ import time
 import json
 
 
+class Response:
+    def __init__(self, input_data, server):
+        self.input_data = input_data
+        self.server = server
+        self.data = self.set()
+
+    @property
+    def input_data(self):
+        return self._input_data
+
+    @input_data.setter
+    def input_data(self, value):
+        if type(value) == bytes:
+            self._input_data = value.decode()
+        else:
+            self._input_data = value
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, value):
+        self._data = value
+
+    @property
+    def as_bytes(self):
+        self.data = bytes(self.data, 'utf-8')
+        return self
+
+    @property
+    def as_json(self):
+        self.data = json.dumps(self.data)
+        return self
+
+    def set(self):
+        match self.input_data:
+            case 'uptime':
+                return f'Server uptime: {round(self.server.uptime, 2)} s'
+            case 'help':
+                return f'Available commands:\n' \
+                       f'help - get the commands list\n' \
+                       f'uptime - get server lifetime\n' \
+                       f'info - get server version\n' \
+                       f'stop - stops both the client and the server\n'
+            case 'info':
+                return f'Server version: {self.server.version}'
+            case 'stop':
+                return None
+        return 'Unknown command'
+
+
 class Server:
     def __init__(self, host, port, version='0.1.0'):
         self.host = host
         self.port = port
-        self.uptime = 0
         self.version = version
         self.time_start = 0
+        self.up = True
+
+    @property
+    def uptime(self):
+        return time.time() - self.time_start
 
     def start(self):
         self.time_start = time.time()
@@ -20,34 +76,16 @@ class Server:
             conn, addr = s.accept()
             with conn:
                 print(f"Connected by {addr}")
-                while True:
-                    self.update_uptime()
+                while self.up:
+                    self.update(conn)
 
-                    client_data = conn.recv(1024)
+    def update(self, conn):
+        client_data = conn.recv(1024)
+        server_response = Response(client_data, self)
 
-                    server_response = self.response_data(client_data)
-                    if not server_response:
-                        break
-                    else:
-                        server_response = json.dumps(server_response)
-                        print(f'Sending response in json: {server_response}')
-                        conn.sendall(bytes(server_response, 'utf-8'))
-
-    def update_uptime(self):
-        self.uptime = time.time() - self.time_start
-
-    def response_data(self, command):
-        match command:
-            case b'uptime':
-                return f'Server uptime: {round(self.uptime, 2)} s'
-            case b'help':
-                return f'Available commands:\n' \
-                       f'help - get the commands list\n' \
-                       f'uptime - get server lifetime\n' \
-                       f'info - get server version\n' \
-                       f'stop - stops both the client and the server\n'
-            case b'info':
-                return f'Server version: {self.version}'
-            case b'stop':
-                return None
-        return 'Unknown command'
+        if not server_response.data:
+            self.up = False
+        else:
+            server_response.as_json
+            print(f'Sending response in json: {server_response.data}')
+            conn.sendall(server_response.as_bytes.data)
