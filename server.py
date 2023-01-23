17a8e9d980f505b63c8d680f1928606bdb7ee685
fifde <fifdee@outlook.com>
2023-01-23 13:08:19 +0100

    User class, Message class, DatabaseManager class, user signup functionality with saving in database (json file), serializing and deserializing data

--- server.py
+++ server.py
@@ -1,6 +1,45 @@
+import datetime
+import re
 import socket
+import string
 import time
 import json
+
+
+class Permission:
+    USER = 'USER',
+    ADMIN = 'ADMIN'
+
+
+class User:
+    def __init__(self, name, password, permission=Permission.USER):
+        self.name = name
+        self.password = password
+        self.permission = permission
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def get_conversations(self):
+        ...
+
+    def get_unread_message(self):
+        ...
+
+    def get_usernames(self):
+        ...
+
+    def send_message(self):
+        ...
+
+
+class Message:
+    def __init__(self, sender, recipient, content, read_by_recipient=False, time_sent=datetime.datetime.now()):
+        self.sender = sender
+        self.recipient = recipient
+        self.content = content
+        self.read_by_recipient = read_by_recipient
+        self.time_sent = time_sent
 
 
 class Response:
@@ -39,20 +78,93 @@ class Response:
         return self
 
     def set(self):
-        match self.input_data:
+        command = self.input_data.split()[0]
+
+        match command:
             case 'uptime':
                 return f'Server uptime: {round(self.server.uptime, 2)} s'
             case 'help':
                 return f'Available commands:\n' \
-                       f'help - get the commands list\n' \
-                       f'uptime - get server lifetime\n' \
-                       f'info - get server version\n' \
-                       f'stop - stops both the client and the server\n'
+                       f'"help" - get the commands list\n' \
+                       f'"login <username> <password>" - log in to the server\n' \
+                       f'"signup <username> <password>" - create new account\n' \
+                       f'"uptime" - get server lifetime\n' \
+                       f'"info" - get server version\n' \
+                       f'"stop" - stops both the client and the server\n'
             case 'info':
                 return f'Server version: {self.server.version}'
             case 'stop':
                 return None
+            case 'signup':
+                return self.user_signup()
         return 'Unknown command'
+
+    def user_signup(self):
+        r = re.compile(r'(signup)\s(\S+)\s(\S+)')
+        match = r.match(self.input_data)
+        if match and len(match.groups()) == 3:
+            print(match.groups())
+            name = match[2]
+            pw = match[3]
+
+            user = User(name, pw)
+            if user in DatabaseManager.get_users():
+                return 'This username is already taken.'
+
+            DatabaseManager.add_user(user)
+            return f'User created: {name}'
+
+
+class DatabaseManager:
+    db = {}
+
+    @classmethod
+    def read_db(cls):
+        try:
+            with open('db.json', 'r') as db:
+                cls.db = json.loads(db.read())
+                cls.deserialize()
+                return cls.db
+        except FileNotFoundError:
+            cls.db = {
+                'users': [],
+                'messages': [],
+            }
+            cls.save_db()
+
+    @classmethod
+    def save_db(cls):
+        with open('db.json', 'w') as db:
+            db.write(json.dumps(cls.serialize()))
+
+    @classmethod
+    def deserialize(cls):
+        cls.db['users'] = [User(user['name'], user['password'], user['permission']) for user in cls.db['users']]
+        cls.db['messages'] = [
+            Message(msg['sender'], msg['recipient'], msg['content'], msg['read_by_recipient'], msg['time_sent']) for msg
+            in cls.db['messages']]
+
+    @classmethod
+    def serialize(cls):
+        cls.db['users'] = [user.__dict__ for user in cls.db['users']]
+        cls.db['messages'] = [message.__dict__ for message in cls.db['messages']]
+        return cls.db
+
+    @classmethod
+    def get_users(cls):
+        return [user for user in cls.db['users']]
+
+    @classmethod
+    def get_messages(cls):
+        return [message for message in cls.db['messages']]
+
+    @classmethod
+    def add_user(cls, user):
+        cls.db['users'].append(user)
+
+    @classmethod
+    def add_message(cls, msg):
+        cls.db['messages'].append(msg)
 
 
 class Server:
@@ -62,6 +174,7 @@ class Server:
         self.version = version
         self.time_start = 0
         self.up = True
+        DatabaseManager.read_db()
 
     @property
     def uptime(self):
@@ -84,6 +197,7 @@ class Server:
         server_response = Response(client_data, self)
 
         if not server_response.data:
+            DatabaseManager.save_db()
             self.up = False
         else:
             server_response.as_json
